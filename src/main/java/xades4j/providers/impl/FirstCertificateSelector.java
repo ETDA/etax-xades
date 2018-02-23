b4aa4c3f988cf854f8c9b86e3f2e75b80f5ae0e2
nathamETDA <natham@NB00271.etda.or.th>
2018-02-23 12:19:35 +0700

    etax-xades version 0.1

--- src/main/java/xades4j/providers/impl/FirstCertificateSelector.java
+++ src/main/java/xades4j/providers/impl/FirstCertificateSelector.java
@@ -23,12 +23,9 @@ import java.util.List;
  *
  * @author Luís
  */
-public class FirstCertificateSelector implements KeyStoreKeyingDataProvider.SigningCertSelector
-{
-    @Override
-    public X509Certificate selectCertificate(
-            List<X509Certificate> availableCertificates)
-    {
-        return availableCertificates.get(0);
-    }
+public class FirstCertificateSelector implements KeyStoreKeyingDataProvider.SigningCertSelector {
+	@Override
+	public X509Certificate selectCertificate(List<X509Certificate> availableCertificates) {
+		return availableCertificates.get(0);
+	}
 }
